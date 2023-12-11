02fc87e81822c368a0dc5cca348b6b7a2ee50a35
Asya Unal <asyaunal02@gmail.com>
2023-12-11 19:10:27 +0300

    contact view updates

--- contacts/src/main/java/com/example/contacts/Controller.java
+++ contacts/src/main/java/com/example/contacts/Controller.java
@@ -21,37 +21,10 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        List<bilkentNumbers> bilkentNumbers = new ArrayList<>(numbers());
-        for (int i = 0; i < bilkentNumbers.size(); i++){
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(getClass().getResource("numbers.fxml"));
 
-            try{
-                HBox hBox = fxmlLoader.load();
-                NumbersController nc = fxmlLoader.getController();
-                nc.setData(bilkentNumbers.get(i));
-                phoneNumbersLayout.getChildren().add(hBox);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
 
     }
 
-    private List<bilkentNumbers> numbers() {
-        List<bilkentNumbers> nums = new ArrayList<>();
 
-        bilkentNumbers num1 = new bilkentNumbers();
-        num1.setName("Bilkent Emergency Call Center");
-        num1.setPhoneNumber("290-6666");
-        nums.add(num1);
-
-        bilkentNumbers num2 = new bilkentNumbers(); // Create a new object for num2
-        num2.setName("Health Center - Main Campus");
-        num2.setPhoneNumber("290-1666");
-        nums.add(num2);
-
-        return nums;
-    }
 
 }
