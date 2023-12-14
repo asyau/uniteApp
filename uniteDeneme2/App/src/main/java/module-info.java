debe127cd83ba6efe4ab47fcbf39be224915439d
keremvarnali <varnalikerem@gmail.com>
2023-12-14 12:39:37 +0300

    Target files added

--- uniteDeneme2/App/src/main/java/module-info.java
+++ uniteDeneme2/App/src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module mainpack.deneme {
 
-    exports mainpack.deneme;
     requires javafx.fxml;
-
     requires javafx.controls;
     requires javafx.graphics;
+    requires java.sql;
+    opens mainpack.deneme to javafx.fxml;
+    exports mainpack.deneme;
 
 }
