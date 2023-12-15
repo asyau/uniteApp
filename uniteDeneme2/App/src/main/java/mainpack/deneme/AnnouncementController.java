30b9621f6a8d755f3d763abc024630b7fed33b9f
keremvarnali <varnalikerem@gmail.com>
2023-12-15 11:32:16 +0300

    Announcement date added

--- uniteDeneme2/App/src/main/java/mainpack/deneme/AnnouncementController.java
+++ uniteDeneme2/App/src/main/java/mainpack/deneme/AnnouncementController.java
@@ -1,14 +1,53 @@
 package mainpack.deneme;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class AnnouncementController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AnnouncementController implements Initializable {
     @FXML
     private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label dateLabel;
+    @FXML
+    private Button weekBackButton;
+    @FXML
+    private Button weekForwardButton;
+
+    private AnnouncementHolder annh;
+    private String dateText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        annh = new AnnouncementHolder(null);
+        setText();
+        weekBackButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                annh.weekBackward();
+                setText();
+            }
+        });
+        weekForwardButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                annh.weekForward();
+                setText();
+            }
+        });
     }
+
+    private void setText() {
+        dateText = String.format("%s - %s", annh.minString, annh.maxString);
+        dateLabel.setText(dateText);
+    }
+
+
+
 }
