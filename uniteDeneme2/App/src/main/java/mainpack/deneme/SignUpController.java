e1a541a6da876a4ffb79e83b56413379a9fa9d6e
Atakan Akar <atakanakar04@hotmail.com>
2023-12-16 16:39:47 +0300

    signup done

--- uniteDeneme2/App/src/main/java/mainpack/deneme/SignUpController.java
+++ uniteDeneme2/App/src/main/java/mainpack/deneme/SignUpController.java
@@ -3,12 +3,17 @@ package mainpack.deneme;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -45,7 +50,18 @@ public class SignUpController implements Initializable {
                 String password = passwordText.getText();
                 signUpIsValid = Authenticator.signUp(mail, password, name);
                 if (signUpIsValid){
-                    //direct to the menu page
+                    try {
+                        Stage stage = (Stage) signUpButton.getScene().getWindow();
+                        stage.close();
+                        Stage primaryStage = new Stage();
+                        Parent root = FXMLLoader.load(getClass().getResource("/menu-view.fxml"));
+                        primaryStage.setTitle("Forum");
+                        primaryStage.setScene(new Scene(root,900,600));
+                        primaryStage.show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        // Handle the exception
+                    }
                 }
             }
         });
