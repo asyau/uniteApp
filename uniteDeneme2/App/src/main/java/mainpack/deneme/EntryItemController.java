3a3a43aa56d5682374adbf0444f3694c31107382
Atakan Akar <atakanakar04@hotmail.com>
2023-12-17 20:03:35 +0300

    some transition done

--- uniteDeneme2/App/src/main/java/mainpack/deneme/EntryItemController.java
+++ uniteDeneme2/App/src/main/java/mainpack/deneme/EntryItemController.java
@@ -6,8 +6,21 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
 
 public class EntryItemController implements Initializable {
 
@@ -17,10 +30,27 @@ public class EntryItemController implements Initializable {
     private Label entryText;
     @FXML
     private ImageView entryImage;
+    @FXML
+    private Button replyButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+    @FXML
+    public void onReplyButtonClick() {
+        try {
+            Stage stage = (Stage) replyButton.getScene().getWindow();
+            stage.close();
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/responseview.fxml"));
+            primaryStage.setTitle("Forum");
+            primaryStage.setScene(new Scene(root,900,600));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception
+        }
     }
 
     public void setData(Question q) {
--- uniteDeneme2/App/src/main/java/mainpack/deneme/ResponseController.java
+++ uniteDeneme2/App/src/main/java/mainpack/deneme/ResponseController.java
@@ -2,13 +2,36 @@ package mainpack.deneme;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import javafx.scene.Parent;
 
 public class ResponseController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button backButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    public void onBackButtonClick() {
+        try {
+            Stage stage = (Stage) backButton.getScene().getWindow();
+            stage.close();
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/forumview.fxml"));
+            primaryStage.setTitle("Forum");
+            primaryStage.setScene(new Scene(root,900,600));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception
+        }
     }
 }
