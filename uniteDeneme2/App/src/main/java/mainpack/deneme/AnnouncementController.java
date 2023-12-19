c7e7b9c39fa39afadc2ecdcd81a3514aa35bb636
Bilgehan <bilgehan.akin@ug.bilkent.edu.tr>
2023-12-19 22:38:09 +0300

    more contacts added and announcements fixed

--- uniteDeneme2/App/src/main/java/mainpack/deneme/AnnouncementController.java
+++ uniteDeneme2/App/src/main/java/mainpack/deneme/AnnouncementController.java
@@ -101,192 +101,145 @@ public class AnnouncementController implements Initializable {
         // Archaeology Club's Event
         cal.set(Calendar.DAY_OF_MONTH, 15);
         cal.set(Calendar.MONTH, 11); // December is the 12th month, so we use 11 as months in Calendar are 0-based
-        cal.set(Calendar.HOUR_OF_DAY, 9);
-        cal.set(Calendar.MINUTE, 0);
         Announcement a3 = new Announcement(cal, "Archaeology Club", "Meeting on International Archaeology Students Conference", "İİSBF/FEASS - C Block Amphi");
         input.add(a3);
 
         // Historical Society's Event
         Calendar cal1 = Calendar.getInstance();
-        cal1.set(Calendar.DAY_OF_MONTH, 25);
+        cal1.set(Calendar.DAY_OF_MONTH, 16);
         cal1.set(Calendar.MONTH, 11);
-        cal1.set(Calendar.HOUR_OF_DAY, 12);
-        cal1.set(Calendar.MINUTE, 30);
         Announcement a4 = new Announcement(cal1, "Historical Society", "Romans in the East, Roman Religion and Unbecoming Roman", "SBF/FEASS C Block Amphi");
         input.add(a4);
 
         // Think Colorfully Club's Event
-        cal.set(Calendar.DAY_OF_MONTH, 27);
+        cal.set(Calendar.DAY_OF_MONTH, 19);
         cal.set(Calendar.MONTH, 11);
-        cal.set(Calendar.HOUR_OF_DAY, 18);
-        cal.set(Calendar.MINUTE, 0);
         Announcement a5 = new Announcement(cal, "Think Colorfully Club", "Queer Awards 2023", "GSMF/FADA FFB 22");
         input.add(a5);
 
         // Beyblade Club's Event
-        cal.set(Calendar.DAY_OF_MONTH, 28);
+        cal.set(Calendar.DAY_OF_MONTH, 20);
         cal.set(Calendar.MONTH, 11);
-        cal.set(Calendar.HOUR_OF_DAY, 18);
-        cal.set(Calendar.MINUTE, 0);
         Announcement a6 = new Announcement(cal, "Beyblade Club", "Second Traditional Beyblade Activity", "MAN V2 Amphi");
         input.add(a6);
 
         // Bilkent Gazette Society's Event
-        cal.set(Calendar.DAY_OF_MONTH, 18);
+        cal.set(Calendar.DAY_OF_MONTH, 21);
         cal.set(Calendar.MONTH, 11);
-        cal.set(Calendar.HOUR_OF_DAY, 17);
-        cal.set(Calendar.MINUTE, 30);
         Announcement a7 = new Announcement(cal, "Bilkent Gazette Society", "Women who Challenge Archetypes in Literature", "HF/FL B Building B 113");
         input.add(a7);
 
         // Management and Economics Society's Event
-        cal.set(Calendar.DAY_OF_MONTH, 26);
+        cal.set(Calendar.DAY_OF_MONTH, 22);
         cal.set(Calendar.MONTH, 11);
-        cal.set(Calendar.HOUR_OF_DAY, 17);
-        cal.set(Calendar.MINUTE, 30);
         Announcement a8 = new Announcement(cal, "Management and Economics Society", "Weekly Club Meeting", "SBF/FEASS B Building BZ 06, BZ 07");
         input.add(a8);
 // Theater Club's Event
-        cal.set(Calendar.DAY_OF_MONTH, 7);
-        cal.set(Calendar.MONTH, 12); // Month is December, 0-based index in Java Calendar is 11
-        cal.set(Calendar.HOUR_OF_DAY, 18);
-        cal.set(Calendar.MINUTE, 30);
+        cal.set(Calendar.DAY_OF_MONTH, 27);
+        cal.set(Calendar.MONTH, 11); // Month is December, 0-based index in Java Calendar is 11
         Announcement a9 = new Announcement(cal, "Theater Club", "Improvisation Exercise", "SBF/FEASS C Block Amphi");
         input.add(a9);
 
 // Outdoor Sports Society's Event
-        cal.set(Calendar.DAY_OF_MONTH, 22);
+        cal.set(Calendar.DAY_OF_MONTH, 28);
         cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 15);
-        cal.set(Calendar.MINUTE, 00);
         Announcement a10 = new Announcement(cal, "Outdoor Sports Society", "Long Climbing Wall Training", "Bilkent Long Climbing Wall");
         input.add(a10);
 
 // Historical Society Seminar Event
-        cal.set(Calendar.DAY_OF_MONTH, 5);
-        cal.set(Calendar.MONTH, 12); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 12);
-        cal.set(Calendar.MINUTE, 30);
+        cal.set(Calendar.DAY_OF_MONTH, 30);
+        cal.set(Calendar.MONTH, 11); // Month is December
         Announcement a11 = new Announcement(cal, "Historical Society", "Romans in the East, Roman Religion and Unbecoming Roman", "SBF/FEASS C Block Amphi");
         input.add(a11);
 
 // Young Leaders Club's Event
-        cal.set(Calendar.DAY_OF_MONTH, 16);
-        cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 10);
-        cal.set(Calendar.MINUTE, 00);
+        cal.set(Calendar.DAY_OF_MONTH, 5);
+        cal.set(Calendar.MONTH, 12); // Month is December
         Announcement a12 = new Announcement(cal, "Young Leaders Club", "Case Study", "GSTMF/FADA FFB 06");
         input.add(a12);
 
 // Musical Society's Rehearsal
-        cal.set(Calendar.DAY_OF_MONTH, 16);
-        cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 10);
-        cal.set(Calendar.MINUTE, 00);
+        cal.set(Calendar.DAY_OF_MONTH, 7);
+        cal.set(Calendar.MONTH, 0); // Month is December
         Announcement a13 = new Announcement(cal, "Musical Society", "Rehearsal", "U Building Old Cafe");
         input.add(a13);
 
 // Theater Club's Second Event
-        cal.set(Calendar.DAY_OF_MONTH, 16);
-        cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 10);
-        cal.set(Calendar.MINUTE, 30);
+        cal.set(Calendar.DAY_OF_MONTH, 11);
+        cal.set(Calendar.MONTH, 0); // Month is December
         Announcement a14 = new Announcement(cal, "Theater Club", "Improvisation Exercise", "SBF/FEASS C Block Amphi");
         input.add(a14);
 
 // Outdoor Sports Society's Training Program
-        cal.set(Calendar.DAY_OF_MONTH, 15);
-        cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 15);
-        cal.set(Calendar.MINUTE, 00);
+        cal.set(Calendar.DAY_OF_MONTH, 13);
+        cal.set(Calendar.MONTH, 0); // Month is December
         Announcement a15 = new Announcement(cal, "Outdoor Sports Society", "Training Program", "Bilkent DOST Long Climbing Wall Training Bilkent Long Climbing Wall");
         input.add(a15);
 
 // QSB Bilkent Student Branch & Physics Society's Movie Screening & Discussion
-        cal.set(Calendar.DAY_OF_MONTH, 15);
-        cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 18);
-        cal.set(Calendar.MINUTE, 00);
+        cal.set(Calendar.DAY_OF_MONTH, 18);
+        cal.set(Calendar.MONTH, 0); // Month is December
         Announcement a16 = new Announcement(cal, "QSB Bilkent Student Branch & Physics Society", "Movie Screening & Discussion: Quantum Computing with Michio Kaku", "HF/FL V2 Amphi");
         input.add(a16);
 
 // Outdoor Sports Society's Training Program
         cal.set(Calendar.DAY_OF_MONTH, 20);
-        cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 15);
-        cal.set(Calendar.MINUTE, 00);
+        cal.set(Calendar.MONTH, 0); // Month is December
         Announcement a17 = new Announcement(cal, "Outdoor Sports Society", "Training Program", "Bilkent DOST Long Climbing Wall Training Bilkent Long Climbing Wall");
         input.add(a17);
 
 // Debate Society's Club Meeting & Practice Match
-        cal.set(Calendar.DAY_OF_MONTH, 20);
-        cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 17);
-        cal.set(Calendar.MINUTE, 40);
+        cal.set(Calendar.DAY_OF_MONTH, 21);
+        cal.set(Calendar.MONTH, 0); // Month is December
         Announcement a18 = new Announcement(cal, "Debate Society", "Club Meeting & Debate Practice Match", "MF/FE B Building B 102");
         input.add(a18);
 
 // Bilkent Gazette Society's Conference
-        cal.set(Calendar.DAY_OF_MONTH, 18);
-        cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 17);
-        cal.set(Calendar.MINUTE, 30);
+        cal.set(Calendar.DAY_OF_MONTH, 13);
+        cal.set(Calendar.MONTH, 0); // Month is December
         Announcement a19 = new Announcement(cal, "Bilkent Gazette Society", "Conference: Women who Challenge Archetypes in Literature, Guest Speaker: Funda Bilgen, Instructor", "HF/FL B Building B 113");
         input.add(a19);
 
 // Management and Economics Society's Club Meeting
-        cal.set(Calendar.DAY_OF_MONTH, 18);
-        cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 17);
-        cal.set(Calendar.MINUTE, 30);
+        cal.set(Calendar.DAY_OF_MONTH, 25);
+        cal.set(Calendar.MONTH, 0); // Month is December
         Announcement a20 = new Announcement(cal, "Management and Economics Society", "Club Meeting: Weekly Club Meeting SBF/FEASS B Building BZ 06, BZ 07", "SBF/FEASS B Building");
         input.add(a20);
 
 // Young Entrepreneurs Society's Club Meeting
-        cal.set(Calendar.DAY_OF_MONTH, 24);
-        cal.set(Calendar.MONTH, 12); // Month is January
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.DAY_OF_MONTH, 26);
+        cal.set(Calendar.MONTH, 0); // Month is January
         Announcement a21 = new Announcement(cal, "Young Entrepreneurs Society", "Club Meeting", "EN Business Talks Amsterdam '24, 2nd Day Amsterdam, Holland");
         input.add(a21);
 
 // Model United Nations Society's Conference
-        cal.set(Calendar.DAY_OF_MONTH, 16);
-        cal.set(Calendar.MONTH, 12); // Month is January
-        cal.set(Calendar.HOUR_OF_DAY, 8);
-        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.DAY_OF_MONTH, 10);
+        cal.set(Calendar.MONTH, 11); // Month is January
+        cal.set(Calendar.YEAR, 2023);
         Announcement a22 = new Announcement(cal, "Model United Nations Society", "Conference MUNBU'24", "MSSF/FMPA Concert Hall, MAZ02, MAZ03, MAZ04, MAZ05, AZ25, AZ27, MA101, MA103, MA104, MA105, MA201, MA202, MA206, MA301, MA302, V01, V02, V03, V04");
         input.add(a22);
 
 // Musical Society's Rehearsal
-        cal.set(Calendar.DAY_OF_MONTH, 17);
-        cal.set(Calendar.MONTH, 12); // Month is January
-        cal.set(Calendar.HOUR_OF_DAY, 18);
-        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.DAY_OF_MONTH, 9);
+        cal.set(Calendar.MONTH, 11); // Month is January
         Announcement a23 = new Announcement(cal, "Musical Society", "Rehearsal", "Mirage Studio, Çankaya Ankara");
         input.add(a23);
 
 // Atelier Bilkent Society & Botanical Society's Activity
-        cal.set(Calendar.DAY_OF_MONTH, 20);
+        cal.set(Calendar.DAY_OF_MONTH, 5);
         cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 18);
-        cal.set(Calendar.MINUTE, 0);
         Announcement a24 = new Announcement(cal, "Atelier Bilkent Society & Botanical Society", "New Years & Christmas Pinecone Decoration Workshop", "U Building Old Cafe");
         input.add(a24);
 
 // Musical Society's Rehearsal
-        cal.set(Calendar.DAY_OF_MONTH, 18);
+        cal.set(Calendar.DAY_OF_MONTH, 23);
         cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 18);
-        cal.set(Calendar.MINUTE, 0);
         Announcement a25 = new Announcement(cal, "Musical Society", "Rehearsal", "MSSF/FMPA Room 215");
         input.add(a25);
 
 // Science Fiction and Fantasy Society's Club Meeting
-        cal.set(Calendar.DAY_OF_MONTH, 18);
+        cal.set(Calendar.DAY_OF_MONTH, 27);
         cal.set(Calendar.MONTH, 11); // Month is December
-        cal.set(Calendar.HOUR_OF_DAY, 18);
-        cal.set(Calendar.MINUTE, 0);
         Announcement a26 = new Announcement(cal, "Science Fiction and Fantasy Society", "Club Meeting", "One Shot Night Main Campus U Building Clubs and Societies Activity Hall");
         input.add(a26);
     }
--- uniteDeneme2/App/src/main/java/mainpack/deneme/ContactController.java
+++ uniteDeneme2/App/src/main/java/mainpack/deneme/ContactController.java
@@ -68,6 +68,25 @@ public class ContactController implements Initializable {
         ImageView ugurDogrusoz = new ImageView("dogrusoz.jpg");
         ImageView calkan = new ImageView("calkan.jpg");
         ImageView saksoy = new ImageView("saksoy.jpg");
+        ImageView aykanat = new ImageView("aykanat.jpg");
+        ImageView shervin = new ImageView("shervin.jpg");
+        ImageView ayhan = new ImageView("ayhan.jpg");
+        ImageView erdal = new ImageView("erdal.jpg");
+        ImageView tolga = new ImageView("tolga.jpg");
+        ImageView mehmet = new ImageView("mehmet.jpg");
+        ImageView burcu = new ImageView("burcu.jpg");
+        ImageView selen = new ImageView("selen.jpg");
+        ImageView bulent = new ImageView("bulent.jpg");
+        ImageView luca = new ImageView("luca.jpg");
+        ImageView yegan = new ImageView("yegan.jpg");
+        ImageView onur = new ImageView("onur.jpg");
+        ImageView ali = new ImageView("ali.jpg");
+        ImageView ilker = new ImageView("ilker.jpg");
+        ImageView akturk = new ImageView("akturk.jpg");
+        ImageView ulku = new ImageView("ulku.jpg");
+        ImageView ozlem = new ImageView("ozlem.jpg");
+        ImageView yigit = new ImageView("yigit.jpg");
+
 
         ArrayList<StaffContact> staffs = new ArrayList<>();
         staffs.add(new StaffContact("Uğur Güdükbay", "EA", "Prof", "CS", "ug@bilkent.edu.tr", ugurGudukbay));
@@ -75,6 +94,25 @@ public class ContactController implements Initializable {
         staffs.add(new StaffContact("Uğur Doğrusöz", "EA", "Prof", "CS", "ud@bilkent.edu.tr", ugurDogrusoz));
         staffs.add(new StaffContact("Can Alkan", "EA", "Assoc", "CS", "ca@bilkent.edu.tr", calkan));
         staffs.add(new StaffContact("Selim Aksoy", "EA", "Doc", "CS", "saksoy@bilkent.edu.tr", saksoy));
+        staffs.add(new StaffContact("Cevdet Aykanat", "EA", "Prof", "CS", "aykanat@cs.bilkent.edu.tr", aykanat));
+        staffs.add(new StaffContact("Shervin R. Arashloo", "EA", "Prof", "CS", "s.rahimzadeh@cs.bilkent.edu.tr", shervin));
+        staffs.add(new StaffContact("Ayhan Altıntaş", "EA", "Prof", "EEE", "altintas@ee.bilkent.edu.tr", ayhan));
+        staffs.add(new StaffContact("Erdal Arıkan", "EE", "Doc", "EEE", "arikan@ee.bilkent.edu.tr", erdal));
+        staffs.add(new StaffContact("Tolga Çukur", "EE", "Prof", "EEE", "cukur@ee.bilkent.edu.tr", tolga));
+        staffs.add(new StaffContact("Mehmet Alper Kutay", "EE", "Doc", "EEE", "kutay@ee.bilkent.edu.tr", mehmet));
+        staffs.add(new StaffContact("Burcu Şenyapılı Özcan", "FF", "Prof", "Arch", "burcu@bilkent.edu.tr", burcu));
+        staffs.add(new StaffContact("Özge Selen Duran", "FF", "Doc", "Arch", "selen.duran@bilkent.edu.tr", selen));
+        staffs.add(new StaffContact("Bülent Batuman", "FA", "Prof", "Arch", "batuman@bilkent.edu.tr", bulent));
+        staffs.add(new StaffContact("Luca Biancofiore", "EA", "Prof", "ME", "luca@bilkent.edu.tr", luca));
+        staffs.add(new StaffContact("Yegan Erdem", "EA", "Doc", "ME", "yeganerdem@bilkent.edu.tr", yegan));
+        staffs.add(new StaffContact("Onur Ozcan", "EA", "Doc", "ME", "onurozcan@bilkent.edu.tr", onur));
+        staffs.add(new StaffContact("Ali Javili", "EA", "Doc", "ME", "ajavili@bilkent.edu.tr", ali));
+        staffs.add(new StaffContact("İlker Temizer", "EA", "Prof", "ME", "temizer@bilkent.edu.tr", ilker));
+        staffs.add(new StaffContact("Selim Aktürk", "EA", "Prof", "IE", "akturk@bilkent.edu.tr", akturk));
+        staffs.add(new StaffContact("Ulku Gürler", "EA", "Prof", "IE", "ulku@bilkent.edu.tr", ulku));
+        staffs.add(new StaffContact("Ozlem Karsu", "EA", "Doc", "IE", "ozlemkarsu@bilkent.edu.tr", ozlem));
+        staffs.add(new StaffContact("Yigit Karpat", "EA", "Doc", "IE", "ykarpat@bilkent.edu.tr", yigit));
+
 
         ContactHolder h = new ContactHolder(staffs);
 
